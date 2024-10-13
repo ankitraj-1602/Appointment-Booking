686bafeb51cc68ecfb396ca4a34477e923f6a4a7
ankitraj-1602 <nycankit@gmail.com>
2024-10-13 13:31:55 +0530

    backend cloudinary and db setup also added routes for admin login and add doctor

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express"
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from "./config/mongodb.js"
+import connectCloudinary from "./config/cloudinary.js"
+import adminRouter from "./routes/adminRoutes.js"
+
+// app config
+const app = express()
+const port = process.env.PORT || 5000
+connectDB()
+connectCloudinary()
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+app.use("/api/admin", adminRouter)
+
+app.get("/", (req, res) => {
+  res.send("API Working")
+});
+
+app.listen(port, () => console.log(`Server started on PORT:${port}`))
