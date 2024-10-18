623e8cdc97b515c3a9a2122f75c4deba3081390b
ankitraj-1602 <nycankit@gmail.com>
2024-10-18 09:54:40 +0530

    admin dashboard

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -11,6 +11,8 @@ const AdminContextProvider = (props) => {
     const [aToken, setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : '')
     const [doctors, setDoctors] = useState([])
     const [appointments, setAppointments] = useState([])
+    const [dashData, setDashData] = useState(false)
+
 
     
 
@@ -88,6 +90,24 @@ const AdminContextProvider = (props) => {
 
     }
 
+    const getDashData = async () => {
+        try {
+
+            const { data } = await axios.get(backendUrl + '/api/admin/dashboard', { headers: { aToken } })
+
+            if (data.success) {
+                setDashData(data.dashData)
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+
+    }
+
     const value = {
         aToken, setAToken,
         doctors,
@@ -96,7 +116,9 @@ const AdminContextProvider = (props) => {
         changeAvailability,
         getAllAppointments,
         cancelAppointment,
-        appointments
+        appointments,
+        dashData,
+        getDashData
     }
 
     return (
